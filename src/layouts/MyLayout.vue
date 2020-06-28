b37387fbcef1c7c9fbf77738bc78df2a7ad1555a
ToussaintAlexander <50060031+ToussaintAlexander@users.noreply.github.com>
2020-06-27 22:35:37 -0400

    Develop (#2)
    * create quasar application
    
    * upgrade project software

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -1,89 +1,105 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+    <q-resize-observer @resize="onResize" />
+    <q-header>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          @click="leftDrawerOpen = !leftDrawerOpen"
-          icon="menu"
-          aria-label="Menu"
-        />
-
         <q-toolbar-title>
-          Quasar App
+          codeguru.io
         </q-toolbar-title>
+        <div class="gt-sm">
+          <q-btn
+            flat
+            rounded
+            label="Courses"
+            class="q-mr-sm"
+          />
+          <q-btn
+            outline
+            rounded
+            label="Sign Up"
+            class="q-mr-sm"
+            :to="{ path: 'signup' }"
+          />
+          <q-btn
+            outline
+            rounded
+            label="Sign In"
+            :to="{ path: 'signin' }"
+          />
+        </div>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div class="lt-md">
+          <q-btn
+            flat
+            dense
+            round
+            @click="leftDrawerOpen = !leftDrawerOpen"
+            icon="menu"
+            aria-label="Menu"
+          />
+
+        </div>
       </q-toolbar>
     </q-header>
-
     <q-drawer
       v-model="leftDrawerOpen"
-      show-if-above
       bordered
       content-class="bg-grey-2"
     >
       <q-list>
         <q-item-label header>Essential Links</q-item-label>
-        <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
+        <q-item
+          clickable
+          tag="a"
+          target="_blank"
+          href="https://quasar.dev"
+        >
           <q-item-section avatar>
             <q-icon name="school" />
           </q-item-section>
           <q-item-section>
-            <q-item-label>Docs</q-item-label>
+            <q-item-label>Courses</q-item-label>
             <q-item-label caption>quasar.dev</q-item-label>
           </q-item-section>
         </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://github.quasar.dev">
+        <q-item
+          clickable
+          tag="a"
+          target="_blank"
+          href="https://github.quasar.dev"
+        >
           <q-item-section avatar>
             <q-icon name="code" />
           </q-item-section>
           <q-item-section>
-            <q-item-label>Github</q-item-label>
+            <q-item-label>Sign Up</q-item-label>
             <q-item-label caption>github.com/quasarframework</q-item-label>
           </q-item-section>
         </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://chat.quasar.dev">
+        <q-item
+          clickable
+          tag="a"
+          target="_blank"
+          href="https://chat.quasar.dev"
+        >
           <q-item-section avatar>
             <q-icon name="chat" />
           </q-item-section>
           <q-item-section>
-            <q-item-label>Discord Chat Channel</q-item-label>
+            <q-item-label>Sign In</q-item-label>
             <q-item-label caption>chat.quasar.dev</q-item-label>
           </q-item-section>
         </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://forum.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="record_voice_over" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Forum</q-item-label>
-            <q-item-label caption>forum.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://twitter.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="rss_feed" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Twitter</q-item-label>
-            <q-item-label caption>@quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://facebook.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="public" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Facebook</q-item-label>
-            <q-item-label caption>@QuasarFramework</q-item-label>
-          </q-item-section>
+        <q-item
+          clickable
+          tag="a"
+          target="_blank"
+          href="https://forum.quasar.dev"
+        >
+
         </q-item>
       </q-list>
     </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -97,6 +113,21 @@ export default {
   data () {
     return {
       leftDrawerOpen: false
+    }
+  },
+  methods: {
+    // openURL,
+    onResize (size) {
+      this.report = size
+    },
+    async signOut () {
+      await this.$Auth
+        .signOut()
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
+      this.signedIn = false
+      parent.signedIn = false
+      this.$router.push({ name: 'signout' })
     }
   }
 }
